307fcd4428321711f309c1cd8585fba56d2a239c
albert <swiftfrog@gmail.com>
2025-10-31 15:05:42 +0800

    cd2.0.1

--- Plugin.cs
+++ Plugin.cs
@@ -2,18 +2,14 @@ using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Serialization;
 using System;
-using System.Collections.Generic;
-using MediaBrowser.Model.Plugins;
 
 namespace OriginalPoster
 {
     /// <summary>
-    /// 插件主类 - 第一阶段基础版本
+    /// 插件主类 - 使用 BasePluginSimpleUI 自动生成配置界面
     /// </summary>
-    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    public class Plugin : BasePluginSimpleUI<PluginConfiguration>
     {
-        // TODO: 使用 uuidgen 命令生成你自己的GUID并替换下面的值
-        // 在Mac终端运行: uuidgen
         public override Guid Id => new Guid("2DE6B212-1C77-EFBC-8B95-A45F6DAE8921");
         
         public override string Name => "TMDB Original Language";
@@ -27,23 +23,6 @@ namespace OriginalPoster
             : base(applicationPaths, xmlSerializer)
         {
             Instance = this;
-            
-            // 第一阶段：启动时输出日志确认插件加载
-            // Console.WriteLine($"[OriginalPoster] Plugin loaded, version {Version}");
- 
-        }
-        
-        // 配置页面（第一阶段先返回空，后续添加）
-        public IEnumerable<PluginPageInfo> GetPages()
-        {
-            return new[]
-            {
-                new PluginPageInfo
-                {
-                    Name = "TMDB Original Language",
-                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.config.html"
-                }
-            };
         }
     }
 }
--- Pluginconfiguration.cs
+++ Pluginconfiguration.cs
@@ -1,32 +1,39 @@
-// Pluginconfiguration.cs
-
 using MediaBrowser.Model.Plugins;
+using System.ComponentModel;
 
 namespace OriginalPoster
 {
     /// <summary>
-    /// 插件配置类 - 第一阶段最简配置
+    /// 插件配置类 - 使用 DisplayName 和 Description 特性来生成友好的 UI
     /// </summary>
     public class PluginConfiguration : BasePluginConfiguration
     {
         /// <summary>
         /// 是否启用插件
         /// </summary>
+        [DisplayName("启用插件")]
+        [Description("是否启用 TMDB 原语言海报功能")]
         public bool Enabled { get; set; }
         
         /// <summary>
         /// 测试模式 - 第一阶段使用，返回测试数据
         /// </summary>
+        [DisplayName("测试模式")]
+        [Description("启用后将返回测试海报，用于验证插件功能")]
         public bool TestMode { get; set; }
         
         /// <summary>
         /// 调试日志
         /// </summary>
+        [DisplayName("调试日志")]
+        [Description("启用后将在控制台输出详细的调试信息")]
         public bool DebugLogging { get; set; }
         
         /// <summary>
         /// 测试用的海报URL（第一阶段使用）
         /// </summary>
+        [DisplayName("测试海报 URL")]
+        [Description("测试模式下使用的海报图片地址")]
         public string TestPosterUrl { get; set; }
         
         public PluginConfiguration()
